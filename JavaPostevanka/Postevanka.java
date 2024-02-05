d4089b146cfafafb5593469cbbe72a494e83e512
Ziga R <ziga.mlearning@gmail.com>
2024-02-05 17:46:26 +0100

    fooly cooly

--- JavaPostevanka/Postevanka.java
+++ JavaPostevanka/Postevanka.java
@@ -13,15 +13,23 @@ public class Postevanka {
     public static void main(String[] args) {
         Random rng = new Random(1337);
         
+        // hyperparameters
+        int nLayers, nHeads, dModel, dHidden, dFF; // for GPT
+        float learningRate; // for optimizer
+        int batchSize, maxEpochs; // for trainer
+        nLayers = 3; nHeads = 2; dModel = 8; dHidden = 4; dFF = 16;
+        learningRate = 1E-3F;
+        batchSize = 50; maxEpochs = 1000; boolean verbose = true;
+
         // train the GPT
         MultiplicationDataset dataset = new MultiplicationDataset(rng);
         GPT model = new GPT(
             dataset.vocabSize(), 
-            3, 2, 8, 4, 16, rng);
-        SGD optim = new SGD(model.parameters(), 1E-3F);
+            nLayers, nHeads, dModel, dHidden, dFF, rng);
+        SGD optim = new SGD(model.parameters(), learningRate);
         CategoricalCrossentropy loss = new CategoricalCrossentropy();
         Trainer trainer = new Trainer(model, loss, optim, dataset);
-        trainer.fit(50, 1000, true);
+        trainer.fit(batchSize, maxEpochs, verbose);
 
         // print out the multiplication table
         GPTMul muller = new GPTMul(model, dataset.tokenizer);
--- JavaPostevanka/Trainer/Trainer.java
+++ JavaPostevanka/Trainer/Trainer.java
@@ -21,20 +21,18 @@ public class Trainer {
 
     public void fit(int batchSize, int maxEpochs, boolean verbose) {
         for (int epoch = 1; epoch <= maxEpochs; epoch++) {
-            float currentLoss = 0;
-            if (verbose) {
-                
-            }
-
+            float epochLoss = 0;
             for (int i = 1; i <= dataset.length(); i++) {
+
+                // gradient accumulation to handle batches
                 Matrix[] batch = dataset.getItem(i);
                 Matrix x = batch[0];
                 Matrix y = batch[1];
                 Matrix pred = model.forward(new Matrix[] {x})[0];
                 Matrix l = loss.forward(new Matrix[] {pred, y})[0];
                 model.backward(loss.backward(new Matrix[] {Matrix.onesLike(l)}));
-                currentLoss += l.get(0, 0) / dataset.length();
-
+                epochLoss += l.get(0, 0) / dataset.length();
+                
                 if (i % batchSize == 0) {
                     optim.step();
                     optim.zeroGrad();
@@ -42,15 +40,14 @@ public class Trainer {
                 
             }
 
-            // TODO: remove if needed
-            if (currentLoss < 0.5) {
-                return;
-            }
+            // empirically proven that it prints out
+            // correct multiplication table if true
+            if (epochLoss < 0.5) return;
 
             if (verbose) {
                 System.out.printf("epoch: %4d, ", epoch);
-                System.out.printf("loss %.3f\n", currentLoss);
-                currentLoss = 0;
+                System.out.printf("loss %.3f\n", epochLoss);
+                epochLoss = 0;
             }
 
         }   
